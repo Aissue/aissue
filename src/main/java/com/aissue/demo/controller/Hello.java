f704a06a67600f0a897234158ed2f78cabf9c8e5
Aissue <1209042946@qq.com>
2017-06-04 00:10:40 +0800

    终于初步完成了一个简单的springmvcDemo

---
+++ src/main/java/com/aissue/demo/controller/Hello.java
@@ -0,0 +1,21 @@
+package com.aissue.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+/**
+ * Created by Aissue on 2017/6/3.
+ *
+ */
+@Controller
+@RequestMapping("/hello")
+public class Hello {
+    @RequestMapping(value = "/index")
+    @ResponseBody
+    public String index() {
+        return "helloindex";
+    }
+
+}
