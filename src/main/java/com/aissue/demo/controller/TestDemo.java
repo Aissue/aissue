5ead982bd9370347f2bf4cbb4b79376870e33697
Aissue <1209042946@qq.com>
2017-06-06 21:14:55 +0800

    设置了log4j的相关配置

--- src/main/java/com/aissue/demo/controller/TestDemo.java
+++ src/main/java/com/aissue/demo/controller/TestDemo.java
@@ -1,7 +1,17 @@
 package com.aissue.demo.controller;
 
+import org.apache.log4j.Logger;
+
 /**
  * Created by Aissue on 2017/6/4.
+ *
  */
 public class TestDemo {
+    private static Logger logger = Logger.getLogger(TestDemo.class);
+    public static void main(String[] args){
+        logger.debug("This is Debug information");
+        logger.error("This is Error information");
+        logger.info("This is just information");
+
+    }
 }
